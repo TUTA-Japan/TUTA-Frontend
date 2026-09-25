0aae1793c71941f3e0c5172b5653e14660cc05f3
thnhdtdev <thnhdat.dev@gmail.com>
2026-09-25 11:00:06 +0700

    ✨  feat: add floating contact buttons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
+import { FloatingContact } from "@/components/floating-contact";
 import "./globals.css";
 
 const montserrat = Montserrat({
@@ -64,6 +65,7 @@ export default function RootLayout({
         <SiteHeader />
         <div className="flex-1">{children}</div>
         <SiteFooter />
+        <FloatingContact />
       </body>
     </html>
   );
